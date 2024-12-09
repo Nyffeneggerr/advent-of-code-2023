c9ae608cf85adf38f2b9c70cf30809c713d64a90
Rolf Nyffenegger <rolf.nyffenegger@xovis.com>
2024-12-09 19:49:58 +0100

    Day 8, part 1

---
+++ src/main/python/2024/day6/day6_1.py
@@ -0,0 +1,49 @@
+
+def read_data():
+    with open('data.txt') as file:
+        return [line.rstrip() for line in file]
+
+
+
+if __name__ == '__main__':
+    data = read_data()
+
+    # testdata
+    # position = [6, 4]
+    # direction = 1
+    position = [94, 73]
+    # directions: 1 = up, 2 = right, 3 = down, 4 = left
+    direction = 1
+    visited = [position]
+
+    while True:
+        row = position[0]
+        col = position[1]
+
+        if direction == 1:
+            row -= 1
+        if direction == 2:
+            col +=1
+        if direction == 3:
+            row += 1
+        if direction == 4:
+            col -= 1
+
+        if row < 0 or col < 0 or row >= len(data) or col >= len(data[0]):
+            break
+
+        new_position = [row , col]
+        char_at_new_pos = data[row][col]
+
+        if '.' in char_at_new_pos:
+            if not new_position in visited:
+                visited.append(new_position)
+            position = new_position
+        else:
+            direction += 1
+            if direction > 4:
+                direction = 1
+
+    result = len(visited)
+
+    print(result)
